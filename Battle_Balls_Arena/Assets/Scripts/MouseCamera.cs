bcd8140e2a6bcbea93e57a09a8a87f78f1da39f8
Wladimir Roberto Barbosa <wladrbarbosa@gmail.com>
2018-09-26 17:39:54 -0300

    Tentativa de fazer a camera seguir a bola sem sofrer rotação

--- Battle_Balls_Arena/Assets/Scripts/MouseCamera.cs
+++ Battle_Balls_Arena/Assets/Scripts/MouseCamera.cs
@@ -16,31 +16,30 @@ public class MouseCamera : MonoBehaviour {
      
     void LateUpdate() {
         if (Input.GetMouseButton(1)) {
-            PositionCameraWithButton();
+            PositionCamera();
+        }
+    }
+
+    void PositionCamera() {
+        float horizontal;
+
+        if (target.GetComponent<Move>().IsMoving) {
+            horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+            target.transform.Rotate(0, horizontal, 0);
+
+            float desiredAngle = target.transform.eulerAngles.y;
+            transform.position = target.transform.position - (offset);
         }
         else {
+            horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+            target.transform.Rotate(0, horizontal, 0);
 
+            float desiredAngle = target.transform.eulerAngles.y;
+            Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
+            transform.position = target.transform.position - (rotation * offset);
         }
-    }
-
-    void PositionCameraWithButton() {
-        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
-        target.transform.Rotate(0, horizontal, 0);
-
-        float desiredAngle = target.transform.eulerAngles.y;
-        Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
-        transform.position = target.transform.position - (rotation * offset);
         
-        transform.LookAt(target.transform);
-    }
-
-    void PositionCameraWithoutButton() {
-        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
-        target.transform.Rotate(0, horizontal, 0);
-
-        float desiredAngle = target.transform.eulerAngles.y;
-        Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
-        transform.position = target.transform.position - (rotation * offset);
+        
         
         transform.LookAt(target.transform);
     }
--- Battle_Balls_Arena/Assets/Scripts/Move.cs
+++ Battle_Balls_Arena/Assets/Scripts/Move.cs
@@ -5,6 +5,59 @@ using UnityEngine;
 public class Move : MonoBehaviour {
 	public Camera MyCamera;
 	public Rigidbody rb;
+	private float noMovementThreshold = 0.001f;
+	private const int noMovementFrames = 3;
+	Vector3[] previousLocations = new Vector3[noMovementFrames];
+	private bool isMoving;
+
+	 //Let other scripts see if the object is moving
+	public bool IsMoving
+	{
+		get{ return isMoving; }
+	}
+	
+	void Awake()
+	{
+		//For good measure, set the previous locations
+		for(int i = 0; i < previousLocations.Length; i++)
+		{
+			previousLocations[i] = Vector3.zero;
+		}
+	}
+	
+	void Update()
+	{
+		//Store the newest vector at the end of the list of vectors
+		for(int i = 0; i < previousLocations.Length - 1; i++)
+		{
+			previousLocations[i] = previousLocations[i+1];
+		}
+		previousLocations[previousLocations.Length - 1] = transform.position;
+	
+		//Check the distances between the points in your previous locations
+		//If for the past several updates, there are no movements smaller than the threshold,
+		//you can most likely assume that the object is not moving
+		for(int i = 0; i < previousLocations.Length - 1; i++)
+		{
+			if(Vector3.Distance(previousLocations[i], previousLocations[i + 1]) >= noMovementThreshold)
+			{
+				//The minimum movement has been detected between frames
+				isMoving = true;
+				break;
+			}
+			else
+			{
+				isMoving = false;
+			}
+		}
+	}
+
+
+
+
+
+
+
 	
 	// Update is called once per frame
 	void FixedUpdate () {
