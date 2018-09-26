2ddedaa849ca9e1fc6cc4c97beee7f3a04e81fc7
Wladimir Roberto Barbosa <wladrbarbosa@gmail.com>
2018-09-26 15:37:59 -0300

    Adição dos arquivos iniciais do projeto

---
+++ Battle_Balls_Arena/Assets/Scripts/MouseCamera.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MouseCamera : MonoBehaviour {
+
+    public GameObject target;
+    public float rotateSpeed = 5;
+    Vector3 offset;
+    bool rightclicked = false;
+     
+    void Start() {
+        offset = target.transform.position - transform.position;
+        PositionCamera();
+    }
+     
+    void LateUpdate() {
+        if (Input.GetMouseButton(1)) {
+            PositionCameraWithButton();
+        }
+        else {
+
+        }
+    }
+
+    void PositionCameraWithButton() {
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        target.transform.Rotate(0, horizontal, 0);
+
+        float desiredAngle = target.transform.eulerAngles.y;
+        Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
+        transform.position = target.transform.position - (rotation * offset);
+        
+        transform.LookAt(target.transform);
+    }
+
+    void PositionCameraWithoutButton() {
+        float horizontal = Input.GetAxis("Mouse X") * rotateSpeed;
+        target.transform.Rotate(0, horizontal, 0);
+
+        float desiredAngle = target.transform.eulerAngles.y;
+        Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
+        transform.position = target.transform.position - (rotation * offset);
+        
+        transform.LookAt(target.transform);
+    }
+}
---
+++ Battle_Balls_Arena/Assets/Scripts/Move.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour {
+	public Camera MyCamera;
+	public Rigidbody rb;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		if (Input.GetMouseButton(0)) {
+			Vector3 movement = MyCamera.transform.forward * 300;
+			rb.AddForce(movement);
+		}
+	}
+}
