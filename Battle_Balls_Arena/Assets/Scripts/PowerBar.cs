6beed63950665fff645297f98896b24991c71c79
Wladimir Roberto Barbosa <wladrbarbosa@gmail.com>
2018-09-27 17:20:00 -0300

    Feito com que a powerbar não surja se o corpo rígido não estiver imóvel

--- Battle_Balls_Arena/Assets/Scripts/PowerBar.cs
+++ Battle_Balls_Arena/Assets/Scripts/PowerBar.cs
@@ -7,10 +7,6 @@ public class PowerBar : MonoBehaviour {
 
 	public Camera MyCamera;
 	public Rigidbody m_Rigidbody;
-	private float noMovementThreshold = 0.04f;
-	private const int noMovementFrames = 3;
-	Vector3[] previousLocations = new Vector3[noMovementFrames];
-    public Vector3 m_Axis;
     public float m_MaxForce;
 
     [Header("Force UI")]
@@ -20,22 +16,6 @@ public class PowerBar : MonoBehaviour {
     public Color m_MinForceColor;
     public Color m_MaxForceColor;
     public AnimationCurve m_Curve;
-	private bool isMoving;
-
-	public bool IsMoving
-	{
-		get{ return isMoving; }
-	}
-	
-	void Awake()
-	{
-		//For good measure, set the previous locations
-		for(int i = 0; i < previousLocations.Length; i++)
-		{
-			previousLocations[i] = Vector3.zero;
-		}
-	}
-
 	public enum ShooterState
     {
         None, Charging, Moving
@@ -45,30 +25,6 @@ public class PowerBar : MonoBehaviour {
 	private float m_ForceDirection = 1;
    	private void Update()
     {
-		//Store the newest vector at the end of the list of vectors
-		for(int i = 0; i < previousLocations.Length - 1; i++)
-		{
-			previousLocations[i] = previousLocations[i+1];
-		}
-		previousLocations[previousLocations.Length - 1] = transform.position;
-	
-		//Check the distances between the points in your previous locations
-		//If for the past several updates, there are no movements smaller than the threshold,
-		//you can most likely assume that the object is not moving
-		for(int i = 0; i < previousLocations.Length - 1; i++)
-		{
-			if(Vector3.Distance(previousLocations[i], previousLocations[i + 1]) >= noMovementThreshold)
-			{
-				//The minimum movement has been detected between frames
-				isMoving = true;
-				break;
-			}
-			else
-			{
-				isMoving = false;
-			}
-		}
-
         if (m_CurrentState == ShooterState.None && Input.GetMouseButtonDown(0))
         {
 			m_ForceSlider.gameObject.SetActive(true);
@@ -89,7 +45,7 @@ public class PowerBar : MonoBehaviour {
             }
         }
 
-        if (m_CurrentState == ShooterState.Charging && Input.GetMouseButtonUp(0))
+		if (m_CurrentState == ShooterState.Charging && Input.GetMouseButtonUp(0))
         {
 			m_CurrentState = ShooterState.Moving;
             float force = m_ForceSlider.value * m_MaxForce;
@@ -99,13 +55,11 @@ public class PowerBar : MonoBehaviour {
     }
 
 	private void LateUpdate ()
-    {
-        if (m_CurrentState == ShooterState.Moving)
-        {
-            if (!IsMoving)
-            {
-                m_CurrentState = ShooterState.None;
-            }
-        }
+    {	
+		if (m_CurrentState == ShooterState.Moving)
+			if (m_Rigidbody.IsSleeping())
+			{
+				m_CurrentState = ShooterState.None;
+			}
     }
 }
